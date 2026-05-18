4ccb6ba8e86418c08536f7ad70f83f7f6dea2317
dk <kudin.dima@gmail.com>
2026-05-18 05:55:37 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Montserrat } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Village Dental NYC | Premium Dental Care',
+  description: 'Experience luxury dental care in NYC, offering routine cleanings and advanced cosmetic solutions in a comfortable, modern environment.',
+};
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${montserrat.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
